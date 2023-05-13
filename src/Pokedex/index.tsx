c509e21c256c210e335f2833ebd30abf07d9a588
Gabriel Conceição <gabrielc2019@outlook.com.br>
2023-05-13 15:29:03 -0300

    fixing another error

--- src/Pokedex/index.tsx
+++ src/Pokedex/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { InputSearch } from '../Components/Input';
 import { usePokemon } from '../Hook/usePokemon';
 import { Pokemons } from '../Components/Pokemons';
