8d9b1f80eb29c5d6663335b86d0a57c129397f14
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-05-04 16:09:30 -0300

    Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,19 @@
-import { ThemeToggle } from '@/styles/theme-toggle';
+import { Link } from 'react-router';
 
+import logo from '@/assets/pokeball.svg';
+import { ThemeToggle } from '@/styles/theme-toggle';
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
-      <ThemeToggle />
+    <header className="flex h-16 max-w-6xl items-center gap-6 border-b px-6">
+      <Link to={'/'} className="flex items-center justify-around gap-6 px-3">
+        <img src={logo} alt="Pokedex logo" className="h-10 w-10" />
+        <h1 className="font-mono text-2xl font-bold text-red-500 dark:text-red-400">
+          Pokedex
+        </h1>
+      </Link>
+      <div className="ml-auto">
+        <ThemeToggle />
+      </div>
     </header>
   );
 }
--- src/pages/_layout/app.tsx
+++ src/pages/_layout/app.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router';
 
-import { Header } from '@/components/Header';
+import { Header } from '@/components/header';
 
 export function AppLayout() {
   return (
-    <div>
+    <div className="mx-auto flex min-h-screen max-w-6xl flex-col antialiased">
       <Header />
-      <Outlet />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+        <Outlet />
+      </div>
     </div>
   );
 }
