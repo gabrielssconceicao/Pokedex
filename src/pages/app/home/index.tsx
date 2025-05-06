862deedf8af0eb6940e16ed7f109910fc2779c6a
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-05-06 15:52:02 -0300

    Home: Footer logic

--- src/pages/app/home/index.tsx
+++ src/pages/app/home/index.tsx
@@ -1,14 +1,8 @@
-import {
-  CaretDoubleLeft,
-  CaretDoubleRight,
-  CaretLeft,
-  CaretRight,
-} from '@phosphor-icons/react';
 import { useQuery } from '@tanstack/react-query';
 
 import { getPokemons } from '@/api/get-pokemons';
-import { Button } from '@/components/ui/button';
 
+import { Pagination } from './pagination';
 import { PokemonCard } from './pokemon-card';
 import { PokemonFilters } from './pokemon-filters';
 export function Home() {
@@ -29,31 +23,12 @@ export function Home() {
           ))
         )}
       </section>
-      <footer className="flex items-center justify-between px-5 py-1">
-        <span className="text-muted-foreground text-sm">Total de 20 items</span>
-        {/*Items per page? */}
-        <div className="flex items-center gap-6 lg:gap-8">
-          <div className="text-muted-foreground text-md">Página 1 de 20</div>
-          <div className="flex items-center gap-2">
-            <Button className="h-8 w-8 p-0">
-              <span className="sr-only">Primeira Página</span>
-              <CaretDoubleLeft className="h-3 w-3" />
-            </Button>
-            <Button className="h-8 w-8 p-0">
-              <span className="sr-only">Página anterior</span>
-              <CaretLeft className="h-3 w-3" />
-            </Button>
-            <Button className="h-8 w-8 p-0">
-              <span className="sr-only">Próxima Página</span>
-              <CaretRight className="h-3 w-3" />
-            </Button>
-            <Button className="h-8 w-8 p-0">
-              <span className="sr-only">Última Página</span>
-              <CaretDoubleRight className="h-3 w-3" />
-            </Button>
-          </div>
-        </div>
-      </footer>
+
+      <Pagination
+        totalItems={pokemons?.count || 0}
+        itemsPerPage={20}
+        pageIndex={0}
+      />
     </main>
   );
 }
---
+++ src/pages/app/home/pagination.tsx
@@ -0,0 +1,59 @@
+import {
+  CaretDoubleLeft,
+  CaretDoubleRight,
+  CaretLeft,
+  CaretRight,
+} from '@phosphor-icons/react';
+
+import { Button } from '@/components/ui/button';
+
+interface PaginationProps {
+  pageIndex: number;
+  totalItems: number;
+  itemsPerPage: number;
+}
+
+export function Pagination({
+  pageIndex,
+  totalItems,
+  itemsPerPage,
+}: PaginationProps) {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  return (
+    <footer className="flex items-center justify-between px-5 py-2">
+      <span className="text-muted-foreground font-mono text-sm">
+        Total de {totalItems} items
+      </span>
+      {/*Items per page? */}
+      <div className="flex items-center gap-6 lg:gap-8">
+        <div className="text-muted-foreground text-md font-mono">
+          Página {pageIndex + 1} de {totalPages}
+        </div>
+        <div className="flex items-center gap-2">
+          <Button className="h-8 w-8 p-0" disabled={pageIndex === 0}>
+            <span className="sr-only">Primeira Página</span>
+            <CaretDoubleLeft className="h-3 w-3" />
+          </Button>
+          <Button className="h-8 w-8 p-0" disabled={pageIndex === 0}>
+            <span className="sr-only">Página anterior</span>
+            <CaretLeft className="h-3 w-3" />
+          </Button>
+          <Button
+            className="h-8 w-8 p-0"
+            disabled={pageIndex === totalPages - 1}
+          >
+            <span className="sr-only">Próxima Página</span>
+            <CaretRight className="h-3 w-3" />
+          </Button>
+          <Button
+            className="h-8 w-8 p-0"
+            disabled={pageIndex === totalPages - 1}
+          >
+            <span className="sr-only">Última Página</span>
+            <CaretDoubleRight className="h-3 w-3" />
+          </Button>
+        </div>
+      </div>
+    </footer>
+  );
+}
