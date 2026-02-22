b1659c919eb8db0e841b6bc52666f931365a1519
Gabriel Conceição <gabrielc2019@outlook.com.br>
2026-02-22 12:04:11 -0300

    feat: pokemon card component finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import { Filters } from './components/filters';
 import { Header } from './components/header';
-import { PokemonCards } from './components/pokemon-cards';
+import { PokemonCard } from './components/pokemon-card';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex flex-1 flex-col">
+      <main className="flex h-full flex-1 flex-col">
         <Filters />
-        <PokemonCards />
+        <section className="flex grow basis-0 flex-wrap items-center justify-around gap-4 overflow-y-auto px-4 py-2">
+          {Array.from({ length: 2 }).map((_, index) => (
+            <PokemonCard key={index} />
+          ))}
+        </section>
+        <h3>2</h3>
       </main>
     </div>
   );
