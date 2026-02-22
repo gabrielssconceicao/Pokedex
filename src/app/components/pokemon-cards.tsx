55d21a42e6b78b1bbf9ef96dd97f48a9d41e5aa7
Gabriel Conceição <gabrielc2019@outlook.com.br>
2026-02-22 09:51:10 -0300

    feat: style home page height

---
+++ src/app/components/pokemon-cards.tsx
@@ -0,0 +1,21 @@
+const pokemons = [
+  {
+    id: 1,
+    name: 'Pikachu',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+    types: ['electric'],
+  },
+
+  {
+    id: 4,
+    name: 'Dragonite',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/149.png',
+    types: ['dragon', 'flying'],
+  },
+];
+
+export function PokemonCards() {
+  return <section className="flex-1 gap-3">1</section>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { Filters } from './components/filters';
 import { Header } from './components/header';
+import { PokemonCards } from './components/pokemon-cards';
 
 export default function Home() {
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       <Header />
-      <main>
+      <main className="flex flex-1 flex-col">
         <Filters />
+        <PokemonCards />
       </main>
-    </>
+    </div>
   );
 }
