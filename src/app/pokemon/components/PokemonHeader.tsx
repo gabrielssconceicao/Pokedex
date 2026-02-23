005029c8c7552f38b05074fdb752acb3878677f1
Gabriel Conceição <gabrielc2019@outlook.com.br>
2026-02-23 10:50:02 -0300

    feat: fix layout header

--- src/app/pokemon/components/PokemonHeader.tsx
+++ src/app/pokemon/components/PokemonHeader.tsx
@@ -6,31 +6,34 @@ import { Badge } from '@/components/ui/badge';
 
 export function PokemonHeader() {
   return (
-    <header className="border-b-foreground flex flex-row items-center justify-between border-b p-1">
-      <Link
-        href={'/'}
-        className="hover:border-primary hover:rounded-lg hover:border"
-        aria-label="Return home"
-      >
-        <span className="sr-only">Home</span>
-        <ArrowLeft size={34} className="text-primary" />
-      </Link>
-      <div className="flex flex-1 flex-col items-center justify-around gap-1 px-2 sm:flex-row sm:gap-3">
-        <div className="flex w-full items-center justify-around gap-2">
+    <header className="border-b-foreground flex items-center border-b p-2">
+      <div className="flex w-12 justify-start">
+        <Link href="/">
+          <span className="sr-only">Home</span>
+          <ArrowLeft size={34} className="text-primary" />
+        </Link>
+      </div>
+
+      <div className="flex flex-1 flex-col items-center justify-center px-2">
+        <div className="flex items-center gap-2">
           <span className="text-accent-foreground text-lg font-semibold">
             #1
           </span>
-          <h1 className="text-accent-foreground justify-center text-lg font-semibold break-all">
+
+          <h1 className="text-accent-foreground text-center text-lg font-semibold break-all">
             Pokemon Details
           </h1>
         </div>
 
-        <div className="flex w-full justify-around gap-2 p-0.5 sm:w-32">
+        <div className="flex w-3/4 min-w-42 gap-2">
           <Badge className="flex-1">Fire</Badge>
-          <Badge className="flex-1">Fire</Badge>
+          <Badge className="flex-1">Flying</Badge>
         </div>
       </div>
-      <ThemeToggle />
+
+      <div className="flex w-12 justify-end">
+        <ThemeToggle />
+      </div>
     </header>
   );
 }
