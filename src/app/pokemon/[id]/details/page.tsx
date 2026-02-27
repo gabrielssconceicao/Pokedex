1e47274aa1530ce1a569a9614fd205ac956548cf
Gabriel Conceição <gabrielc2019@outlook.com.br>
2026-02-27 15:12:26 -0300

    feat: species card component

--- src/app/pokemon/[id]/details/page.tsx
+++ src/app/pokemon/[id]/details/page.tsx
@@ -8,28 +8,35 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 
+import { SpeciesCardDespription } from './components/SpeciesCardDescription';
+
+const species = {
+  description:
+    ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt iste voluptates doloribus ipsa vel impedit suscipit, animi quod!',
+  height: '2\'4\" (0.71 m)',
+  weight: '10 lbs (4.5 kg)',
+};
+
 export default function Details() {
   return (
     <section>
-      <div className="flex flex-col items-center justify-center gap-3 px-2 py-3">
-        <aside className="flex w-3/4 items-center justify-center rounded-md bg-amber-400 p-3">
+      <div className="flex flex-col items-center justify-center gap-3 px-2 py-3 sm:flex-row sm:items-stretch">
+        <aside className="flex w-full items-center justify-center rounded-md bg-amber-400 p-3 sm:w-3/4">
           <Image src={'/pokemon-egg.png'} width={80} height={80} alt="1" />
         </aside>
         <aside className="w-full px-3 py-4">
-          <Card>
+          <Card className="py-2">
             <CardHeader className="border-b-2">
               <CardTitle className="text-center">Species</CardTitle>
             </CardHeader>
             <CardContent className="grid grid-cols-2 gap-3">
-              <CardDescription className="border-accent-foreground/50 text-accent-foreground col-span-2 rounded-md border px-2 py-1 text-center">
-                Descrpition
-              </CardDescription>
-              <CardDescription className="border-accent-foreground/50 text-accent-foreground col-span-1 rounded-md border px-2 py-1 text-center">
-                Weight
-              </CardDescription>
-              <CardDescription className="border-accent-foreground/50 text-accent-foreground col-span-1 rounded-md border px-2 py-1 text-center">
-                Height
-              </CardDescription>
+              <SpeciesCardDespription
+                label="Description"
+                value={species.description}
+                colSpan={2}
+              />
+              <SpeciesCardDespription label="Weight" value={species.weight} />
+              <SpeciesCardDespription label="Height" value={species.height} />
             </CardContent>
           </Card>
         </aside>
